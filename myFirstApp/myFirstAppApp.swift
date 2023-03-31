5f9620e06b2ba254172d892989410379caab6a43
Adriano Araujo Valumi <adriano.valumin@outlook.com>
2023-03-31 19:54:57 -0300

    Initial Commit

---
+++ myFirstApp/myFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstAppApp.swift
+//  myFirstApp
+//
+//  Created by Adriano Valumin on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct myFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
