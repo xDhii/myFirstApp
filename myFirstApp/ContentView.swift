61e734d7346f3c16e1583f21a1251fe2df3967c8
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2023-04-01 21:20:19 -0300

    Initial commit

--- myFirstApp/ContentView.swift
+++ myFirstApp/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showAlert = false
+    @State var changeText = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            HStack {
+                Spacer()
+
+                Button(action: {
+                    self.changeText = !self.changeText
+                }) {
+                    Text("Botão 1")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("Primary Button"))
+                }
+
+                Spacer()
+
+                Button(action: {
+                    self.changeText = !self.changeText
+                }) {
+                    Text("Botão 2")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("Secondary Button"))
+                }
+
+                Spacer()
+
+            }.padding(.vertical, 20)
+
+            Spacer()
+
+            changeText ?
+                Text("Agora é para mostrar")
+                .fontWeight(.bold)
+                .foregroundColor(Color.green)
+                :
+                Text("Agora é para NÃO MOSTRAR")
+                .fontWeight(.bold)
+                .foregroundColor(Color.red)
+
+            Spacer()
+
+            Button(action: {
+                self.changeText = !self.changeText
+            }) {
+                Text("Clique aqui")
+                    .font(.subheadline)
+                    .foregroundColor(Color("Primary Text"))
+            }
+
+            Spacer()
         }
-        .padding()
     }
 }
 
